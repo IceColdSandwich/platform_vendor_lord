4c9adb5c7280394d653c257790434569154bba35
Rob Beane <robbeane@gmail.com>
2011-12-05 13:34:50 -0500

    import squisher and modver from CM

--- talon-vendor.mk
+++ talon-vendor.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product-if-exists, vendor/talon/products/common_versions.mk)
+
 # Google Apps
 $(call inherit-product-if-exists, vendor/google/google-vendor.mk)
 
