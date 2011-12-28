0f8f749be191865852118e97d024c9b75ab789ad
Davor Bertovic <davor@losinj.com>
2011-12-28 23:32:06 +0100

    Increase build speed

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ lunch lord_ace-eng
 export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
-#make -j6 lord
-make CC=gcc-4.4 CXX=g++-4.4 -j6 lord
+#make -j10 lord
+make CC=gcc-4.4 CXX=g++-4.4 -j10 lord
