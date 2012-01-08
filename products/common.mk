4945ea82c1075c2d9c7507dca2d5e67eefd0559f
Davor Bertovic <davor@losinj.com>
2012-01-08 15:12:07 +0100

    talon -> lord

--- products/common.mk
+++ products/common.mk
@@ -1,10 +1,10 @@
-# Generic Talon product
+# Generic Lord product
 PRODUCT_NAME := lord
 PRODUCT_BRAND := lord
 PRODUCT_DEVICE := generic
 
 # Include GSM stuff
-$(call inherit-product, vendor/talon/products/gsm.mk)
+$(call inherit-product, vendor/lord/products/gsm.mk)
 
 PRODUCT_PACKAGES += \
     Superuser \
@@ -31,24 +31,24 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 
 PRODUCT_COPY_FILES += \
-    vendor/talon/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
-    vendor/talon/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
-    vendor/talon/prebuilt/common/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
-    vendor/talon/prebuilt/common/etc/terminfo/u/unknown:system/etc/terminfo/u/unknown \
-    vendor/talon/prebuilt/common/etc/profile:system/etc/profile \
-    vendor/talon/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/talon/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-    vendor/talon/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/talon/prebuilt/common/etc/init.d/04modules:system/etc/init.d/04modules \
-    vendor/talon/prebuilt/common/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
-    vendor/talon/prebuilt/common/etc/init.d/06mountdl:system/etc/init.d/06mountdl \
-    vendor/talon/prebuilt/common/etc/init.d/20userinit:system/etc/init.d/20userinit \
-    vendor/talon/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions \
-    vendor/talon/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/talon/prebuilt/common/bin/verify_cache_partition_size.sh:system/bin/verify_cache_partition_size.sh \
-    vendor/talon/prebuilt/common/xbin/htop:system/xbin/htop \
-    vendor/talon/prebuilt/common/xbin/irssi:system/xbin/irssi \
-    vendor/talon/prebuilt/common/xbin/powertop:system/xbin/powertop \
-    vendor/talon/prebuilt/common/xbin/openvpn-up.sh:system/xbin/openvpn-up.sh
+    vendor/lord/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/lord/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/lord/prebuilt/common/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
+    vendor/lord/prebuilt/common/etc/terminfo/u/unknown:system/etc/terminfo/u/unknown \
+    vendor/lord/prebuilt/common/etc/profile:system/etc/profile \
+    vendor/lord/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/lord/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    vendor/lord/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/lord/prebuilt/common/etc/init.d/04modules:system/etc/init.d/04modules \
+    vendor/lord/prebuilt/common/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
+    vendor/lord/prebuilt/common/etc/init.d/06mountdl:system/etc/init.d/06mountdl \
+    vendor/lord/prebuilt/common/etc/init.d/20userinit:system/etc/init.d/20userinit \
+    vendor/lord/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions \
+    vendor/lord/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/lord/prebuilt/common/bin/verify_cache_partition_size.sh:system/bin/verify_cache_partition_size.sh \
+    vendor/lord/prebuilt/common/xbin/htop:system/xbin/htop \
+    vendor/lord/prebuilt/common/xbin/irssi:system/xbin/irssi \
+    vendor/lord/prebuilt/common/xbin/powertop:system/xbin/powertop \
+    vendor/lord/prebuilt/common/xbin/openvpn-up.sh:system/xbin/openvpn-up.sh
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/talon/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/lord/overlay/common
