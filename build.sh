4cdda53ca7ade1ffc51dd1c6f1b75b53596b078b
Davor Bertovic <davor@losinj.com>
2012-01-09 21:08:45 +0100

    Build with CM repo

--- build.sh
+++ build.sh
@@ -2,9 +2,12 @@ export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
 source build/envsetup.sh
-lunch lord_ace-eng
+lunch cm_ace-eng
 export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
-#make -j8 lord
-make CC=gcc-4.4 CXX=g++-4.4 -j8 lord
+#make -j6 lord
+
+
+#build on ne linux
+make CC=gcc-4.4 CXX=g++-4.4 -j6 lord
--- products/lord_ace.mk
+++ products/lord_ace.mk
@@ -1,8 +1,8 @@
 # Inherit AOSP device configuration for ace
-$(call inherit-product, device/htc/ace/ace.mk)
+$(call inherit-product, device/htc/ace/cm.mk)
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/talon/products/common.mk)
+$(call inherit-product, vendor/lord/products/common.mk)
 
 # Google Apps
 $(call inherit-product-if-exists, vendor/google/google-vendor.mk)
@@ -20,7 +20,6 @@ PRODUCT_DEVICE := ace
 PRODUCT_MODEL := Desire HD
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=google/yakju/maguro:4.0.1/ITL41D/223971:user/release-keys PRIVATE_BUILD_DESC="yakju-user 4.0.1 ITL41D 223971 release-keys"
-#PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=htc_wwe/htc_ace/ace:2.3.3/GRI40/87995:user/release-keys PRIVATE_BUILD_DESC="2.50.405.2 CL87995 release-keys"
 
 # Lord Version
 PRODUCT_VERSION_MAJOR = 0
