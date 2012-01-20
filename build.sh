6d40133ba33941758223373088cda011daae1fef
Davor Bertovic <davor@losinj.com>
2012-01-20 16:18:05 +0100

    fix build after last changes

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
 source build/envsetup.sh
-lunch lord_ace-eng
+lunch htc_ace-eng
 export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
