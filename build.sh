c7b7997b1a043d6b3baf54e1a2756697080453f7
Davor Bertovic <davor@losinj.com>
2012-01-22 22:11:44 +0100

    let's build with 4.6.3 toolchain - thanks to existz

--- build.sh
+++ build.sh
@@ -9,5 +9,7 @@ prebuilt/linux-x86/ccache/ccache -M 40G
 #make -j6 lord
 
 
-#build on ne linux
-make CC=gcc-4.4 CXX=g++-4.4 -j6 lord
+#build on new linux
+#make CC=gcc-4.4 CXX=g++-4.4 -j6 lord
+
+make -j6 lord TARGET_TOOLS_PREFIX=prebuilt/linux-x86/toolchain/arm-eabi-4.6.3/bin/arm-linux-androideabi-
