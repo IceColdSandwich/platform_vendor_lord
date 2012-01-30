5eeb5848d57b196f8c96ef6881a8479894cf4115
Davor Bertovic <davor@losinj.com>
2012-01-30 18:00:27 +0100

    fix otapackage :)

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ lunch htc_ace-eng
 export USE_CCACHE=1
 export CCACHE_DIR=/home/lord/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
-#make -j6 lord
+make -j6 lord
 
 
 #build on new linux
-make CC=gcc-4.4 CXX=g++-4.4 -j6
+# make CC=gcc-4.4 CXX=g++-4.4 lord -j6
