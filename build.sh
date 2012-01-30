ea1d5a69d91e11cc0b7cb69d9dff00e316f70f38
tualatin <simmeone@gibts.net>
2012-01-30 13:27:06 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 export USE_CCACHE=1
-export CCACHE_DIR=/home/lord/.ccache
+export CCACHE_DIR=/$HOME/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
 source build/envsetup.sh
 lunch htc_ace-eng
 export USE_CCACHE=1
-export CCACHE_DIR=/home/lord/.ccache
+export CCACHE_DIR=/$HOME/.ccache
 prebuilt/linux-x86/ccache/ccache -M 40G
 #make -j6 lord
 
