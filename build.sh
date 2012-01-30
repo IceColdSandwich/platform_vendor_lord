e5bac4cb03002030317bb84e9c8a89cd29aad772
Davor Bertovic <davor@losinj.com>
2012-01-30 16:44:22 +0100

    no need for toolchain path flag anymore

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ prebuilt/linux-x86/ccache/ccache -M 40G
 
 
 #build on new linux
-make CC=gcc-4.4 CXX=g++-4.4 -j6 lord TARGET_TOOLS_PREFIX=prebuilt/linux-x86/toolchain/arm-eabi-4.6.3/bin/arm-linux-androideabi-
+make CC=gcc-4.4 CXX=g++-4.4 -j6
